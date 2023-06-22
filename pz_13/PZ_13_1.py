b8807e3cd385d700a4395125e1a610cfef2e8f55
NastyaAnonova <114396383+NastyaAnonova@users.noreply.github.com>
2023-06-23 00:27:38 +0300

    Create PZ_13_1.py

---
+++ pz_13/PZ_13_1.py
@@ -0,0 +1,22 @@
+Matr2 = [[1, 2, 3, 4],
+         [5, 6, 7, 8],
+         [9, 10, 11, 12],
+         [13, 14, 15, 16]]
+
+n_rows = len(Matr2)
+n_cols = len(Matr2[0])
+
+Matr1 = []
+for i in range(1, n_rows - 1):
+    row = []
+    for j in range(1, n_cols - 1):
+        row.append(Matr2[i][j])
+    Matr1.append(row)
+
+print('Исходная матрица:')
+for row in Matr2:
+    print(row)
+
+print('Новая матрица:')
+for row in Matr1:
+    print(row)
