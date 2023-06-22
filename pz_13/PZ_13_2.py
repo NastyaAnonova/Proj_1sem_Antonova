31569be9b200718e8f4131930238451e440efce2
NastyaAnonova <114396383+NastyaAnonova@users.noreply.github.com>
2023-06-23 00:28:23 +0300

    Create PZ_13_2.py

---
+++ pz_13/PZ_13_2.py
@@ -0,0 +1,12 @@
+matrix = [[1, 2, 3, -4],
+          [5, -6, 7, 8],
+          [-9, 10, -11, 12],
+          [13, -14, 15, -16]]
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] < 0:
+            matrix[i][j] = matrix[i][j] ** 2
+
+for row in matrix:
+    print(row)
