497acf076c46d773f914fca4de51dda3bd1a4673
NastyaAnonova <114396383+NastyaAnonova@users.noreply.github.com>
2023-06-23 00:26:25 +0300

    Create PZ_11_2.py

---
+++ pz_11/PZ_11_2.py
@@ -0,0 +1,18 @@
+with open('text18-1.txt', 'r') as file:
+    content = file.read()
+    print(content)
+    upper_count = sum(1 for c in content if c.isupper())
+
+print(f'Количество букв в верхнем регистре: {upper_count}')
+
+with open('text18-1.txt', 'r') as file:
+    lines = file.readlines()
+
+with open('poem.txt', 'w') as f:
+    
+    f.write(lines[0] + '\n')
+    
+    f.write(lines[-1] + '\n')
+    
+    for line in lines[1:-1]:
+        f.write('\t' + line + '\n')
